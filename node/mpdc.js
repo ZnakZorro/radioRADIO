483849ac4b0f33f8ec34291156b0c4673dd0dce9
Znak Zorro <ZnakZorro@users.noreply.github.com>
2017-01-30 17:43:15 +0100

    Update mpdc.js

--- node/mpdc.js
+++ node/mpdc.js
@@ -1,7 +1,7 @@
 #!/usr/local/bin/node
 
-var mpd = require('/usr/local/lib/node_modules/mpd'),
-cmd = mpd.cmd;
+var mpd = require('/usr/local/lib/node_modules/mpd');
+var cmd = mpd.cmd;
 
 var client = mpd.connect({
 	port: 6600,
@@ -9,8 +9,7 @@ var client = mpd.connect({
 });
 
 client.on('ready', function() {
-	console.log("ready");
-	cmd('play','32');
+	console.log("ready --------------");
 });
 client.on('system', function(name) {
 	console.log("update", name);
@@ -19,13 +18,17 @@ client.on('system', function(name) {
 client.on('system-player', function() {
 	client.sendCommand(cmd("status", []), function(err, msg) {
 		if (err) throw err;
+		console.log('system-player');
 		console.log(msg);
 	});
 });
 
-var nr=32;
+var nr=33;
 setInterval(function(){
-	cmd('play',nr);
 	nr++;
-	console.log(nr);
-},10000);
+	client.sendCommand(cmd("play", [nr]), function(err, msg) {
+		if (err) throw err;
+		console.log(msg);
+		console.log('--------Interval cmd=',nr);
+	});
+},15000);
