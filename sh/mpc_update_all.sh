791ccf7be9c26a2bebbc9f6b8e418acb73cdba3b
Znak Zorro <ZnakZorro@users.noreply.github.com>
2017-02-01 13:43:47 +0100

    add descriptions

--- sh/mpc_update_all.sh
+++ sh/mpc_update_all.sh
@@ -1,15 +1,26 @@
+echo " "
+echo "..............................."
+echo " "
+echo "Update MPC ......."
 mpc clear
-sleep 1
 mpc update
 sleep 1
+echo " "
+echo "Load radio ......."
 mpc load radio
 sleep 1
+echo " "
+echo "Add Music/muzyka ......."
 mpc add /home/pi/Music/muzyka/
 sleep 5
+echo " "
+echo "Save mpc.m3u ......."
 mpc rm mpc
-sleep 1
 mpc save mpc
-sleep 1
+echo " "
+echo "Start Play 1 ......."
 mpc play 1
 
 echo "=======- KONIEC MPC UPDATE -=========="
+echo " "
+echo " "
