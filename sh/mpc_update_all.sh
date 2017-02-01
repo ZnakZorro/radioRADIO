d9a73914f24095b40a7dbedddab2e7bb6640c93d
Znak Zorro <ZnakZorro@users.noreply.github.com>
2017-02-01 11:09:46 +0100

    add sleeps

--- sh/mpc_update_all.sh
+++ sh/mpc_update_all.sh
@@ -1,9 +1,15 @@
 mpc clear
+sleep 1
 mpc update
+sleep 1
 mpc load radio
+sleep 1
 mpc add /home/pi/Music/muzyka/
+sleep 5
 mpc rm mpc
+sleep 1
 mpc save mpc
+sleep 1
 mpc play 1
 
 echo "=======- KONIEC MPC UPDATE -=========="
