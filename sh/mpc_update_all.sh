5cf7045659444ec52f4b998a5d95eae492ed49f0
Znak Zorro <ZnakZorro@users.noreply.github.com>
2017-02-02 07:42:43 +0100

    add > /dev/null

--- sh/mpc_update_all.sh
+++ sh/mpc_update_all.sh
@@ -2,24 +2,27 @@ echo " "
 echo "..............................."
 echo " "
 echo "Update MPC ......."
-mpc clear
-mpc update
+mpc clear > /dev/null
+mpc update > /dev/null
 sleep 1
-echo " "
+
 echo "Load radio ......."
-mpc load radio
+mpc load radio > /dev/null
 sleep 1
-echo " "
+
 echo "Add Music/muzyka ......."
-mpc add /home/pi/Music/muzyka/*
+mpc add /home/pi/Music/muzyka/* > /dev/null
 sleep 5
-echo " "
+
 echo "Save mpc.m3u ......."
-mpc rm mpc
-mpc save mpc
-echo " "
+mpc rm mpc > /dev/null
+mpc save mpc > /dev/null
+
 echo "Start Play 1 ......."
 mpc play 1
+mpc playlist
+echo " "
+mpc play
 
 echo "=======- KONIEC MPC UPDATE -=========="
 echo " "
