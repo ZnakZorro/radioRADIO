78a7bd558893f74da08e4f865bb5d93e19e21131
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-02-23 08:18:15 +0100

    Create M_oidar.js

---
+++ oidar/M_oidar.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+	
+header: function(title) {
+return `<html>
+<head>	
+<meta charset="utf-8">	
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
+<title>${title}</title>	
+<style>
+body{font:normal 12pt verdana; margin:0.5%;}
+div {float:left; margin:1%;padding:1%; width:90%;}
+div.klawisze  {}
+div.container {width:91%; margin:1% 3%; border:1px solid #ddd; background:#f8f8f8;  border-radius:0.5em;}
+a {float:left; text-decoration:none; color:#124; border:1px solid gray; background:#e0e0e0; padding:0.5em; margin:0.5em; border-radius:0.5em;}
+</style>
+</head>
+<body>
+`;
+},
+
+div: function(id,wpis) {
+	return `<div id="${id}">${wpis}</div>`;
+},
+
+footer: function(wpis) {
+	return `<div><hr />${wpis}</div></body></html>`;
+}
+  
+
+};
