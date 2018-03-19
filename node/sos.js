5eb541e371f6ce5ba28f19d7cbe78e62db35ff21
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-03-19 08:31:42 +0100

    add more css & command

--- node/sos.js
+++ node/sos.js
@@ -17,26 +17,42 @@ res.write(`
 <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1, maximum-scale=1">
 <meta name="mobile-web-app-capable" content="yes">
 <link rel="shortcut icon" href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAT0EyAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAABAAAAAAAAAAAQAAAAAAAAAAEAAAAAAAAAABAAAAAAAAAAAQAAAAAAAAAAEAAAAAAAAAABAAAAAAAAAAAQAAAAAAAAAAEAAAAAAAAAABAAAAAAAAAAAQAAAAAAAAAAEAAAAAAAAAABAAAAAAAAAAAQAAAAAAAAAAF//wAAv/8AAN//AADv/wAA9/8AAPv/AAD9/wAA/v8AAP9/AAD/vwAA/98AAP/vAAD/9wAA//sAAP/9AAD//gAA" />
-<style>body{font:normal 11pt verdana} a{text-decoration:none;}</style>
+<style>
+body{font:normal 17px verdana} a{text-decoration:none; margin:0;}
+div.wrap {background:white; position:relative;}
+div {margin:1%; padding:0.5%;border-bottom:1px solid #e8e8e8; clear:both; background:#f8f8f8;}
+div.main {max-width:800px; margin:auto;}
+a {text-align:center; padding:0.33em; margin:0.2em; background:#ddd;line-height:1.5em;display: inline-block; border:1px solid gray; border-radius:0.5em;min-width:5.8em;}
+</style>
 </head>
 <body>
+<div class="wrap">
+<div class="main">
 <h3>SOS</h3>
-<div>
-<a href="/">Start</a><br />
-<a href="/mpc/play">Play</a><br />
-<a href="/mpc/stop">Stop</a><br />
-<a href="/mpc/current">Info</a><br />
-<a href="/mpc/play/1">TOK FM</a><br />
-<a href="/mpc/play/2">RMF Classic</a><br />
-<a href="/mpc/play/61">Radio Classic</a><br />
-<hr />
-<a href="/ls/-l">LS</a><br />
-<a href="/df/-h">DF</a><br />
-<div>
-<hr />
-<div>${command}<div>
-<hr />
-<div>${message}<div>
+	<div>
+		<a href="/">Start</a>
+		<a href="/mpc/play">Play</a>
+		<a href="/mpc/stop">Stop</a>
+		<a href="/mpc/current">Info</a>
+		<a href="/mpc/play/1">TOK FM</a>
+		<a href="/mpc/play/2">RMF Classic</a>
+		<a href="/mpc/play/61">Classic 61</a>
+		<a href="/mpc/play/57">Classic 57</a>
+	</div>
+
+	<div>
+		<a href="/reboot">reboot</a>
+		<a href="/poweroff">poweroff</a>
+		<a href="/ls/-l">LS</a>
+		<a href="/df/-h">DF</a>
+		<a href="/temp">Temp</a>
+		<a href="/wifi">WIFI</a>
+		<a href="/cpu">CPU</a>
+	</div>
+	<div>${command}</div>
+	<div>${message}</div>
+</div>
+</div>
 </body></html>	
 `);
 res.end(skrypt+'\n');	
@@ -64,19 +80,33 @@ http.createServer((req, res) => {
 					}
 				}
 			}
+			if (cmd ==='reboot')   command='sudo reboot';
+			if (cmd ==='poweroff') command='sudo poweroff';
+			if (cmd ==='temp')     command='/opt/vc/bin/vcgencmd measure_temp';
+			if (cmd ==='wifi')     command='cat /proc/net/wireless';
+			if (cmd ==='cpu')      command="grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "+'"%"'+"}'";
+			 
+
 			//-----------------------------------------------
 	
 	if (command){
 		console.log('command=',command);
-		exec(command,function(a,info,c){
-			console.log(a)
-			console.log(info)
-			console.log(c)
-			if (info) message += '<pre>'+info+'</pre>';
-			sendPage(res,message,command,skrypt);
-		});
+		try {
+			exec(command,function(err, stdout, stderr){
+				console.log('#err=',err);
+				console.log('#stdout=',stdout);
+				console.log('#stderr=',stderr);
+				let info = '';
+				if (stdout) info += stdout;
+				if (stderr) info += stderr;
+				message += '<pre>'+info+'</pre>';
+				sendPage(res,message,command,skrypt);
+			});
+		} catch(err){console.error(0,err);}
 	} else {
-		sendPage(res,message,command,skrypt);
+		try {
+			sendPage(res,message,command,skrypt);
+		} catch(err){console.error(1,err);}
 	}
 
 	
